a27f09aeee7942e8c5ef659d89916c2e4a0c0d28
r4m1r2z <ooscar_leo@hotmail.com>
2018-04-23 17:32:50 -0500

    Define la funcion a través de la cual se crea el tablero de una sopa de letras, de acuerdo a unos parametros establecidos por el usuario (numero de filas y columnas)

---
+++ CrearJuego.py
@@ -0,0 +1,17 @@
+def CREAR_SOPA(x,y):
+    """
+
+    :param x:
+    :param y:
+    :return:
+    """
+    contador = 0
+    matriz = []
+    while (contador < x):
+        contador2 = 0
+        matriz.append([str(input("Ingrese una letra: "))])
+        while(contador2 < y - 1):
+            matriz[contador].append([str(input("Ingrese una letra: "))])
+            contador2 = contador2 + 1
+        contador = contador + 1
+    return matriz
